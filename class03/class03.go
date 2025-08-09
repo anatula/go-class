54ead5d417531dafc2f3d4752e7826413ef7ca0c
Ana Laura Tula <analaura.tula@gmail.com>
2025-08-10 01:01:48 +0200

    class03 type conversions

--- class03/class03.go
+++ class03/class03.go
@@ -7,6 +7,17 @@ func main() {
 	b := 2.1
 	// %T type of
 	// %v value of
-	fmt.Printf("a: %T %v \n", a, a)
-	fmt.Printf("b: %T %v \n", b, b)
+	// to align columns %8T
+	fmt.Printf("a: %8T %v \n", a, a)
+	// put varible once
+	fmt.Printf("b: %8T %[1]v \n", b)
+
+	a = int(b) // float to int the fractional is removed
+	fmt.Printf("a: %8T %[1]v \n", a)
+
+	b = float64(a) // float to int the fractional is removed
+	fmt.Printf("b: %8T %[1]v \n", b)
+
+	// types of a and b can't change anymore
+	// fractional part goes away in convertion and lost
 }
