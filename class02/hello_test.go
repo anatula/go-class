a97a7a55f8c98071b63a68895989a11fb1d1ab4c
Ana Laura Tula <analaura.tula@gmail.com>
2025-08-10 00:21:12 +0200

    test various cases using structs

--- class02/hello_test.go
+++ class02/hello_test.go
@@ -1,6 +1,9 @@
 package hello
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // start with Test
 // always take a pointer to a testing.T
@@ -18,11 +21,33 @@ import "testing"
 // }
 
 func TestSayHello(t *testing.T) {
-	want := "Hello, test!"
-	got := Say([]string{"test"})
+	// create a slice of structs
+	// create it on the fly, didn't give it an actual type name
+	// create the first subtest, don't give it all the fields (items is empty to test default behaviour)
+	// various cases to test (not actual subtests)
+	subtests := []struct {
+		items  []string
+		result string
+	}{
+		{
+			result: "Hello, world!",
+		},
+		{
+			items:  []string{"Ana"},
+			result: "Hello, Ana!",
+		},
+		{
+			items:  []string{"Ana", "Matt"},
+			result: "Hello, Ana, Matt!",
+		},
+	}
 
-	if want != got {
-		t.Errorf("wanted %s, got %s", want, got)
+	// loop over the subtests, one at a time
+	for _, st := range subtests {
+		if s := Say(st.items); s != st.result {
+			fmt.Printf("DEBUG: actual result '%s' of items (%v) with expected result: '%s'", s, st.items, st.result)
+			t.Errorf("wanted %s (%v), got %s", st.result, st.items, s)
+		}
 	}
 
 }
