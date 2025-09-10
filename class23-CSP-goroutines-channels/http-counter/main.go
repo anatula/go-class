10063a58c96e03aff442106c8bdb64a776992bc4
Ana Laura Tula <analaura.tula@gmail.com>
2025-09-11 00:41:32 +0200

    not supper efficient but safe web counter

--- class23-CSP-goroutines-channels/http-counter/main.go
+++ class23-CSP-goroutines-channels/http-counter/main.go
@@ -6,7 +6,21 @@ import (
 	"net/http"
 )
 
-var nextID int
+// convert nextID from variable to a channel of int
+// instead of increment, I read a value out of it
+// i need a function with job is to start sending numbers
+// var nextID int
+
+var nextID = make(chan int)
+
+// generate numbers and put it in the channel
+// i can't write to channel until someone is ready to be read from it
+// if not ready to be read from you block (like waiting stdin user typing)
+func counter() {
+	for i := 0; ; i++ {
+		nextID <- i
+	}
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Only handle the root path, ignore favicon requests
@@ -14,11 +28,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+	// reader can't read until somebody is ready to write
+	// not a problem from the handler because the loop is not waiting for anything
+	// as soon as someone "i want a the webpage with counter", try to read and counter will be able to write the next value
+	// and counter will stop and handler will finish, do this over and over
 	fmt.Fprintf(w, "<h1> You got %d <h1>", nextID)
-	nextID++
+	//nextID++
 }
 
 func main() {
+	// go routine, runs forever until program stops
+	go counter()
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
