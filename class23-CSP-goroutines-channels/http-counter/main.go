d87e301139a78a96b48cd929bd3f3f9c8c2866d3
Ana Laura Tula <analaura.tula@gmail.com>
2025-09-11 00:47:39 +0200

    Object Oriented version of program not supper efficient but safe web counter

--- class23-CSP-goroutines-channels/http-counter/main.go
+++ class23-CSP-goroutines-channels/http-counter/main.go
@@ -6,23 +6,27 @@ import (
 	"net/http"
 )
 
+// NEW SOLUTION: turn handler method with receiver channel
+
 // convert nextID from variable to a channel of int
 // instead of increment, I read a value out of it
 // i need a function with job is to start sending numbers
 // var nextID int
 
-var nextID = make(chan int)
+//var nextID = make(chan int)
+
+type nextCh chan int
 
 // generate numbers and put it in the channel
 // i can't write to channel until someone is ready to be read from it
 // if not ready to be read from you block (like waiting stdin user typing)
-func counter() {
+func counter(ch chan<- int) {
 	for i := 0; ; i++ {
-		nextID <- i
+		ch <- i
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func (ch nextCh) handler(w http.ResponseWriter, r *http.Request) {
 	// Only handle the root path, ignore favicon requests
 	if r.URL.Path != "/" { //favicon
 		http.NotFound(w, r)
@@ -32,13 +36,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// not a problem from the handler because the loop is not waiting for anything
 	// as soon as someone "i want a the webpage with counter", try to read and counter will be able to write the next value
 	// and counter will stop and handler will finish, do this over and over
-	fmt.Fprintf(w, "<h1> You got %d <h1>", nextID)
+	fmt.Fprintf(w, "<h1> You got %d <h1>", <-ch)
 	//nextID++
 }
 
 func main() {
 	// go routine, runs forever until program stops
-	go counter()
-	http.HandleFunc("/", handler)
+	var nextID nextCh = make(chan int)
+	go counter(nextID)
+	http.HandleFunc("/", nextID.handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
