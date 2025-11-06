6e96398fe3f50dd0dd4a03333ffd419f71632e9e
Ana Laura Tula <analaura.tula@gmail.com>
2025-11-06 22:06:43 +0100

    1st fix all the errors for first exercise

--- class33-reflection/exercise/main.go
+++ class33-reflection/exercise/main.go
@@ -18,12 +18,12 @@ type respWrapper struct {
 }
 
 var j1 = `{
-	"item": "album"
+	"item": "album",
 	"album": {"title": "Dark Side of the Moon"}
 }`
 
 var j2 = `{
-	"item": "song"
+	"item": "song",
 	"song": {"title": "Bella Donna", "artist": "Stevie Nicks"}
 }`
 
@@ -40,9 +40,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%#v\n", resp1.response)
+	fmt.Printf("%#v\n", resp2.response)
 }
 
+// needs this name because it's implementing the json.Unmarshaler
+// func (r *respWrapper) UnmarshalJSON(b []byte) error
 func (r *respWrapper) UnmarshalJSON(b []byte) (err error) {
 	// can represent any json object
 	// strings are the properties of the json object
@@ -52,27 +54,31 @@ func (r *respWrapper) UnmarshalJSON(b []byte) (err error) {
 	// the only thing that is gonna unmarshall into response
 	// only "item" since it's the only with a json tag
 	// not gonna find anything else because they are all capital names and my data has lower case
-
 	err = json.Unmarshal(b, &r.response)
 	// give me the map of string to interface and go fishing inside that based on the type of the item
 	err = json.Unmarshal(b, &raw)
 
 	switch r.Item {
+
 	case "album":
+		// idem was of type "album" i went and found the data for the key in this map of string to interface
+		// which gave another map of string to interface and if I find the "title" in it, the put title into r
+		// which is my response wrapperbut but it embeds a response
+		// so i'm really setting the album field in the response
 		inner, ok := raw["album"].(map[string]interface{})
 		if ok {
-			album, ok := inner["title"].(string); ok {
+			if album, ok := inner["title"].(string); ok {
 				r.Album = album
 			}
 		}
 
 	case "song":
-				inner, ok := raw["song"].(map[string]interface{})
+		inner, ok := raw["song"].(map[string]interface{})
 		if ok {
-			title, ok := inner["title"].(string); ok {
+			if title, ok := inner["title"].(string); ok {
 				r.Title = title
 			}
-			artist, ok := inner["title"].(string); ok {
+			if artist, ok := inner["artist"].(string); ok {
 				r.Artist = artist
 			}
 		}
